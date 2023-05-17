fa371439e55cb45448c3ea0f754859f989f14856
Francis Tan <kian080787@gmail.com>
2023-05-17 19:57:12 +0800

    ac

--- django_line_bot/urls.py
+++ django_line_bot/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 # from django.conf.urls import url
-from echobot import views
+# from echobot import views
 
 
 urlpatterns = [
--- echobot/urls.py
+++ echobot/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
+# from django.urls import path
+# from . import views
 
-from . import views
-
-urlpatterns = [
-    path('callback/', views.callback, name='callback'),
-]
+# urlpatterns = [
+#     path('callback/', views.callback),
+# ]
