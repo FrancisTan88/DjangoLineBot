a78399c85dc03ef8d448a5f089165aa1e41e818f
Francis Tan <kian080787@gmail.com>
2023-05-17 20:18:44 +0800

    ac

--- django_line_bot/urls.py
+++ django_line_bot/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 # from django.conf.urls import url
 from echobot import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # url('^callback',views.callback),
-    path('callback/', views.callback),
+    re_path('^callback', views.callback),
+    # path('callback/', views.callback),
 ]
