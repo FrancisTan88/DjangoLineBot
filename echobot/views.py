560a7fe3f1c24a4734c79f428dffe226e245429b
Francis Tan <kian080787@gmail.com>
2023-05-19 00:27:39 +0800

    modify

--- echobot/views.py
+++ echobot/views.py
@@ -104,6 +104,8 @@ def callback(request):
                     elif "基本資訊" in mtext:
                         stock_id = str(mtext.split(" ")[0])
                         basic_info = scrawl_info(stock_id)
+                        if len(basic_info) >= 500:
+                            basic_info = basic_info[:500]
                         line_bot_api.reply_message(
                             event.reply_token,
                             TextSendMessage(text=basic_info))
